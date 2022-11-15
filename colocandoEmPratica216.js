e11b876440151ed3b21c80f660d126b9ba63bacf
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2022-11-15 17:21:01 -0300

    adicionando desafio var, let e const

--- colocandoEmPratica216.js
+++ colocandoEmPratica216.js
@@ -1,14 +1,37 @@
+// ESCOPO
+
+// function confirmarDados(nome){
+//     var nomeCompleto = nome + ' da Silva';
+//     var idade = 20;
+//     function getAnoNascimento(){
+//         var ano = 2022 - idade;
+//         return ano
+//     }
+
+//     console.log('nome completo:', nomeCompleto);
+//     console.log('ano de nascimento:', getAnoNascimento());
+//     console.log('idade:', idade);
+// }
+
+// confirmarDados('Marcos');
+
+VAR, LET E CONST 
+
 function confirmarDados(nome){
-    var nomeCompleto = nome + ' da Silva';
-    var idade = 20;
+    const nomeCompleto = nome + ' da Silva';
+    let geracao = 'millenium';
     function getAnoNascimento(){
-        var ano = 2022 - idade;
+        let idade = 40;
+        let ano = 2022 - idade;
         return ano
     }
-
+    const anoDeNascimento = getAnoNascimento();
+    if(anoDeNascimento <= 1995){
+        geracao = 'geração X';
+    }
     console.log('nome completo:', nomeCompleto);
-    console.log('ano de nascimento:', getAnoNascimento());
-    console.log('idade:', idade);
+    console.log('ano de nascimento:', anoDeNascimento);
+    console.log('geração:', geracao);
 }
+confirmarDados('Marcos')
 
-confirmarDados('Marcos');
